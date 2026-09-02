a84ebb97f9af7ee3a547191ddfd63796e529177c
Marcosa <marcos.ezequiel.villada@gmail.com>
2026-09-02 02:52:11 +0000

    Puse un bucle while al menu y corregi unos bugs

--- EPNro1/consolidar.sh
+++ EPNro1/consolidar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 while true
 do
-  cd "$HOME/tarea-bash/EPNro1/entrada"
+  cd "$HOME/Tarea-bash/EPNro1/entrada"
   for archivo in *.txt
   do
     if [[ -f "$archivo" ]]; then
-       cat "$archivo" >> "$HOME/tarea-bash/EPNro1/salida/$FILENAME.txt"
-       mv $archivo $HOME/tarea-bash/EPNro1/procesado/
-       echo "$(date +"%d/%m/%Y %H:%M:%S") - Procesado archivo $archivo" >> "$HOME/tarea-bash/EPNro1/procesado.log"
+       cat "$archivo" >> "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt"
+       mv $archivo $HOME/Tarea-bash/EPNro1/procesado/
+       echo "$(date +"%d/%m/%Y %H:%M:%S") - Procesado archivo $archivo" >> "$HOME/Tarea-bash/EPNro1/procesado.log"
     fi
   done
   sleep 5
--- menu.sh
+++ menu.sh
@@ -7,57 +7,63 @@ if [[ "$1" == "-d" ]]; then
    exit
 fi
 
-echo "1) Crear entorno"
-echo "2) Correr Proceso"
-echo "3) Mostrar listado"
-echo "4) Mostrar 10 notas"
-echo "5) Solicitar Informacion"
-echo "6) Visualizar Log"
-echo "7) Salir"
+while true
+do
+  echo "1) Crear entorno"
+  echo "2) Correr Proceso"
+  echo "3) Mostrar listado"
+  echo "4) Mostrar 10 notas"
+  echo "5) Solicitar Informacion"
+  echo "6) Visualizar Log"
+  echo "7) Salir"
+  echo "==============================="
+  echo -n  "Elegi una opcion: "
+  read opcion
+  echo ""
 
-read opcion
-
-case $opcion in
-    1)
-      mkdir -p $HOME/Tarea-bash/EPNro1/entrada $HOME/Tarea-bash/EPNro1/salida $HOME/Tarea-bash/EPNro1/procesado
+  case $opcion in
+      1)
+        mkdir -p $HOME/Tarea-bash/EPNro1/entrada $HOME/Tarea-bash/EPNro1/salida $HOME/Tarea-bash/EPNro1/procesado
+        echo "Entorno creado"
+        ;;
+      2)
+        $HOME/Tarea-bash/EPNro1/consolidar.sh &
+        echo "Proceso corriendo en segundo plano"
+        ;;   
+      3)
+        if [[ -f "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt" ]]; then
+           sort -k1 -n $HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt
+        else
+           echo "el archivo no existe"
+        fi
+        ;;
+      4)
+        if [[ -f "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt" ]]; then
+           sort -k5 -n -r $HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt | head -10
+        else
+           echo "el archivo no existe"
+        fi
+        ;;
+      5)
+        if [[ -f "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt" ]]; then
+           echo "ingrese numero de padron: "
+           read padron
+           grep "$padron" $HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt
+        else
+           echo "el archivo no existe"
+        fi 
       ;;
-    2)
-      $HOME/Tarea-bash/EPNro1/consolidar.sh &
-      ;;   
-    3)
-      if [[ -f "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt" ]]; then
-         sort -k1 -n $HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt
-      else
-          echo "el archivo no existe"
-      fi
+      6)
+        cat $HOME/Tarea-bash/EPNro1/procesado.log
       ;;
-    4)
-      if [[ -f "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt" ]]; then
-         sort -k5 -n -r $HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt | head -10
-      else
-         echo "el archivo no existe"
-      fi
-      ;;
-    5)
-      if [[ -f "$HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt" ]]; then
-         echo "ingrese numero de padron: "
-         read padron
-         grep "$padron" $HOME/Tarea-bash/EPNro1/salida/$FILENAME.txt
-      else
-         echo "el archivo no existe"
-      fi 
-      ;;
-    6)
-      cat $HOME/Tarea-bash/EPNro1/procesado.log
-      ;;
-    7)
-      echo "Saliendo del menu"
-      exit
-      ;;
-    *)
-      echo "Opcion invalida"
-      ;;
-esac
-
+      7)
+        echo "Saliendo del menu"
+        exit 0
+        ;;
+      *)
+        echo "Opcion invalida"
+        ;;
+  esac
+done
 
 
